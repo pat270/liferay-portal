61324c3f10e76c5aa377adde635cdd54edd9e3d7
Kevin Tan <kevin.tan@liferay.com>
2016-01-08 13:56:27 -0800

    LRQA-12004 Clean portlet java file

--- modules/apps/test-ui-taglibs/test-ui-taglibs-portlet/src/main/java/com/liferay/test/ui/taglib/portlet/TestUITaglibPortlet.java
+++ modules/apps/test-ui-taglibs/test-ui-taglibs-portlet/src/main/java/com/liferay/test/ui/taglib/portlet/TestUITaglibPortlet.java
@@ -16,46 +16,27 @@ package com.liferay.test.ui.taglib.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
-import java.io.IOException;
-
-import java.util.Map;
-
 import javax.portlet.Portlet;
-import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Modified;
-import org.osgi.service.component.annotations.Reference;
 
+/**
+ * @author Kevin Tan
+ */
 @Component(
-	configurationPid = "",
-	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
+	immediate = true,
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-test-ui-taglib",
-		"com.liferay.portlet.display-category=category.tools",
+		"com.liferay.portlet.display-category=category.test",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		// "com.liferay.portlet.icon=/icons/currency_converter.png",
-		"com.liferay.portlet.preferences-owned-by-group=false",
-		"com.liferay.portlet.private-request-attributes=false",
-		"com.liferay.portlet.private-session-attributes=false",
-		"com.liferay.portlet.remoteable=true",
 		"com.liferay.portlet.render-weight=0",
 		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Test UI Taglibs",
 		"javax.portlet.expiration-cache=0",
-		// "javax.portlet.init-param.edit-guest-template=/edit.jsp",
-		// "javax.portlet.init-param.edit-template=/edit.jsp",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.portlet-mode=text/html;edit,edit-guest",
-		"javax.portlet.resource-bundle=content.Language"
 	},
 	service = Portlet.class
 )
 public class TestUITaglibPortlet extends MVCPortlet {
-
 }
